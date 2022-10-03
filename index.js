94f7fe0877f1d8c50f397bac04ad9e49c78239f3
Angelina Stavniiciuc <angiestavnk@gmail.com>
2022-10-03 10:30:33 +0300

    scroll behaviour optimization

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.$$ = document.querySelectorAll.bind(document);
 window.on = window.addEventListener;
 
 const frameDistance = 100;
-let scena, frames;
+let scene, frames;
 
 const setupInitialFramesPosition = () => {
   frames.forEach((frame, frameIndex) => {
@@ -16,21 +16,30 @@ const setupInitialFramesPosition = () => {
   })
 }
 
+const framesPrescroll = () => {
+  setTimeout(() => {
+    window.scrollTo({ top: 1 })
+  }, 10)
+}
+
 window.on('load', () => {
-  scena = $('scene');
+  scene = $('#scene');
 
   frames = $$('.scene-frame');
   setupInitialFramesPosition();
+
+  framesPrescroll();
 })
+
+const setSceneScrollPosition = (scrollPosition) => {
+  scene.style.setProperty(
+    '--scroll-position',
+    `${scrollPosition}px`
+  )
+}
 
 window.on('scroll', () => {
   const scrollPosition = Math.floor(document.documentElement.scrollTop);
-  console.log(`Scroll: ${scrollPosition}`);
 
-  frames.forEach((frame) => {
-    frame.style.setProperty(
-      '--scroll-position',
-      `${scrollPosition}px`
-    )
-  })
+  setSceneScrollPosition(scrollPosition);
 })
