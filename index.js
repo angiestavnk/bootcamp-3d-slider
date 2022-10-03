27fec906daa8974ec4b72959704ca0b80ff22444
Angelina Stavniiciuc <angiestavnk@gmail.com>
2022-10-03 12:46:40 +0300

    css scroll optimization

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ const { throttle, debounce } = throttleDebounce;
 const frameDistance = 100;
 let scene, frames, fullSliderLength;
 
+const scrollModule = (scene, fullSliderLength) => {
+  const optimizationCssClass = "scene-frame-optimization"
+  let optimization = false;
+  let scrollTimerId = undefined;
+
+  const beforeAction = () => {
+    if(!optimization) {
+      scene.classList.add(optimizationCssClass);
+      optimization = true;
+      console.log('before scroll')
+    }
+  };
+
+  const afterAction = () => {
+    clearTimeout(scrollTimerId);
+    scrollTimerId = setTimeout(() => {
+      optimization = false;
+      scene.classList.remove(optimizationCssClass);
+      console.log('After scroll')
+    }, 1000)
+  }
+
+  const scrollHandler = () => {
+
+    beforeAction();
+    const scrollPosition = Math.floor(document.documentElement.scrollTop);
+    const correctedScrollPosition = proportionalPageScrollPosition(scrollPosition, fullSliderLength);
+
+    setSceneScrollPosition(scene, correctedScrollPosition);
+
+    afterAction()
+  }
+
+  return {
+    scrollHandler: scrollHandler
+  }
+}
+
 window.on('load', () => {
   scene = $('#scene');
 
@@ -11,14 +49,6 @@ window.on('load', () => {
   setupInitialFramesPosition(frames, frameDistance);
 
   framesPrescroll();
+
+  window.on('scroll', throttle(100, scrollModule(scene, fullSliderLength).scrollHandler))
 })
-
-
-const scrollHandler = () => {
-  const scrollPosition = Math.floor(document.documentElement.scrollTop);
-  const correctedScrollPosition = proportionalPageScrollPosition(scrollPosition, fullSliderLength);
-
-  setSceneScrollPosition(scene, correctedScrollPosition);
-}
-
-window.on('scroll', throttle(100, scrollHandler))
